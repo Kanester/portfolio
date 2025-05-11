38efde2a93ad60ecc600944f0ed9d4f19e4743fb
KanesterP <pampagkieven091@gmail.com>
2025-05-11 21:44:59 +0800

    hello

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('sveltejs/kit').Config} **/
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		scss: {
+			includePaths: ['node_modules']
+		}
+	}),
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
