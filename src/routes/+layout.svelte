650cc6335d77261e296a95c7510c10a0bf9041b9
KanesterP <pampagkieven091@gmail.com>
2025-05-12 17:31:31 +0800

    hrhe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,18 +13,20 @@
 </script>
 
 <header class="container-fluid">
-  <nav class="desktop-nav">
-    <ul>
-      <li><strong>Kiov</strong></li>
-    </ul>
-    <ul>
-      {#each links as link}
-      <li>
-        <a href={base + link} sveltekit:prefetch class:contrast={current == link} class:secondary={current != link}>{link == "/" ? "Home" : link.slice(1).toUpperCase()}</a>
-      </li>
-      {/each}
-    </ul>
-  </nav>
+  <div class="desktop-nav">
+    <nav>
+      <ul>
+        <li><strong>Kiov</strong></li>
+      </ul>
+      <ul>
+        {#each links as link}
+          <li>
+            <a href={base + link} sveltekit:prefetch class:contrast={current == link} class:secondary={current != link}>{link == "/" ? "Home" : link.slice(1).toUpperCase()}</a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
+  </div>
 
   <div class="mobile-nav">
     <nav>
