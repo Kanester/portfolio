ab0e3dfa8c70a0d763a8a3ea953ae014751f8701
KanesterP <pampagkieven091@gmail.com>
2025-05-12 23:15:16 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,23 @@
-<section class="container" data-theme="dark">
-  <hgroup>
-    <h1>Hi y'all! I'm Kiö.</h1>
-    <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow.. (&#x2060;&#x665;&#x2060;&#x21BC;&#x2060;_&#x2060;&#x21BC;&#x2060;)</p>
-    <p>Programmer, storyteller, and CSS is a nightmare.</p>
-    <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
-    <p><i>Tell me I'm wrong</i> &#x2192;<a class="contrast" href="#contact">Contact</p>
-  </hgroup>
-    
-  <div class="grid">
-    <a role="button" href="blog">Blog</a>
-    <a role="button" href="projects">Projects</a>
+<section data-theme="dark">
+  <div class="container">
+    <hgroup>
+      <h1>Hi y'all! I'm Kiö.</h1>
+      <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow.. (&#x2060;&#x665;&#x2060;&#x21BC;&#x2060;_&#x2060;&#x21BC;&#x2060;)</p>
+      <p>Programmer, storyteller, and CSS is a nightmare.</p>
+      <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
+      <p><i>Tell me I'm wrong</i> &#x2192;<a class="contrast" href="#contact">Contact</p>
+    </hgroup>
+      
+    <div class="grid">
+      <a role="button" href="blog">Blog</a>
+      <a role="button" href="projects">Projects</a>
+    </div>
   </div>
 </section>
 
-<section class="container about" data-theme="light">
+<section class="about" data-theme="light">
+  <div class="container">
     <h1>About Me!</h1>
     <p>Still under construction...</p>
+  </div>
 </section>
