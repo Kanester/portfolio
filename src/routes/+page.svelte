2ac8ffefa6b88995e28d2e7cbea9e3034dc5fddb
KanesterP <pampagkieven091@gmail.com>
2025-05-12 19:43:48 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,21 @@
 <div class="container" data-theme="dark">
-  <h1>Hi y'all! I'm Kane.</h1>
-  <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow...</p>
-  <p>Programmer, storyteller, and CSS is a nightmare.</p>
-  <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
-  <p><i>Tell me I'm wrong</i> &#x2192;<a class="contrast" href="#contact">Contact</p>
+  <hgroup>
+    <h1>Hi y'all! I'm Kane.</h1>
+    <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow...</p>
+    <p>Programmer, storyteller, and CSS is a nightmare.</p>
+    <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
+    <p><i>Tell me I'm wrong</i> &#x2192;<a class="contrast" href="#contact">Contact</p>
+  </hgroup>
   
-  <div class="container">
-    <a role="button" href="blog">Blog</a>
-    <a role="button" href="projects">Projects</a>
+  <div class="grid">
+    <div>
+      <a role="button" href="blog">Blog</a>
+      <a role="button" href="projects">Projects</a>
+    </div>
   </div>
 </div>
 
-<br/>
+<hr/>
 
 <div class="container" data-theme="light">
   <h1>About Me!</h1>
