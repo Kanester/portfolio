e999e9c90ec5060c4a8e169a9f3b4206b9811a60
KanesterP <pampagkieven091@gmail.com>
2025-05-12 16:11:54 +0800

    hrhe

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <header class="container-fluid">
-  <nav class="desktop">
+  <nav class="desktop-nav">
     <ul>
       <li><strong>Kiov</strong></li>
     </ul>
@@ -26,29 +26,31 @@
     </ul>
   </nav>
 
-  <nav class="phone">
-    <ul>
-      <li><strong on:click={clicked}>&#9776;</strong></li>
-    </ul>
+  <div class="mobile-nav">
+    <nav>
+      <ul>
+        <li><strong on:click={clicked}>&#9776;</strong></li>
+      </ul>
     
-    <ul>
-      <li><strong>Kiov</strong></li>
-    </ul>
+      <ul>
+        <li><strong>Kiov</strong></li>
+      </ul>
     
-    <ul>
-      <li><strong>KV</strong></li>
-    </ul>
-  </nav>
+      <ul>
+        <li><strong>KV</strong></li>
+      </ul>
+    </nav>
   
-  <aside class:hidden={!isClicked} class="container-fluid">
-    <ul>
-      {#each links as link}
-      <li>
-        <a class:contrast={current == link} class:secondary={current != link} href={base + link} sveltekit:prefetch>{link == "/" ? "Home" : link.slice(1).toUpperCase()}</a>
-      </li>
-      {/each}
-    </ul>
-  </aside>
+    <aside class:hidden={!isClicked}>
+      <ul>
+        {#each links as link}
+          <li>
+            <a class:contrast={current == link} class:secondary={current != link} href={base + link} sveltekit:prefetch>{link == "/" ? "Home" : link.slice(1).toUpperCase()}</a>
+          </li>
+        {/each}
+      </ul>
+    </aside>
+  </div>
 </header>
 
 <main class="container-fluid">
@@ -58,11 +60,27 @@
 <footer class="container-fluid">&copy; KanesterP</footer>
 
 <style lang="scss">
-  .desktop {
-    display: none;
+  @media (min-width: var(--pico-breakpoints-sm)) {
+    .desktop-nav {
+      display: none;
+    }
+    
+    .mobile-nav {
+      display: block;
+      
+      .hidden {
+        display: none;
+      }
+    }
   }
   
-  .hidden {
-    display: none;
+  @media (min-width: var(--pico-breakpoints-md)) {
+    .desktop-nav {
+      display: block;
+    }
+    
+    .mobile-nav {
+      display: none;
+    }
   }
 </style>
