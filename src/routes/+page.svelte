8996d71a5298cc6bb18f1f1bb0cb6898381cbe2f
KanesterP <pampagkieven091@gmail.com>
2025-05-12 19:09:05 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
 <div class="container" data-theme="dark">
-  <h1>Hi y&#x27;all! I&#x27;m Ki&#xF6;</h1>
+  <h1>Hi y'all! I'm Kane.</h1>
+  <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work.</p>
+  <p>Programmer, storyteller, and CSS is a nightmare.</p>
+  <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing.</p>
+  <p>Tell me I'm wrong &rarr; <a href="/contact">Contact</a></p>
 </div>
