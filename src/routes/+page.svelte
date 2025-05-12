367ad5bbce1c863e7355b53bcc9649078b98f8ba
KanesterP <pampagkieven091@gmail.com>
2025-05-12 19:29:38 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <div class="container" data-theme="dark">
-  <h1>Hi y'all! I'm Kane.</h>
-  <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work—somehow...</p>
+  <h1>Hi y'all! I'm Kane.</h1>
+  <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow...</p>
   <p>Programmer, storyteller, and CSS is a nightmare.</p>
-  <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing... <i>Tell me I'm wrong</i></p>
+  <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
+  <i>Tell me I'm wrong &#x2192; <a class="contrast" href="#contact">Contact</a></i>
   <a role="button" href="blog">Blog</a>
   <a role="button" href="projects">Projects</a>
 </div>
