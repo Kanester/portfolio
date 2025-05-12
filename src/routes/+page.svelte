362589484ee56e227827a23ab481d5b368f55dc2
KanesterP <pampagkieven091@gmail.com>
2025-05-12 19:32:41 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,15 @@
   <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow...</p>
   <p>Programmer, storyteller, and CSS is a nightmare.</p>
   <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
-  <i>Tell me I'm wrong &#x2192; <a class="contrast" href="#contact">Contact</a></i>
-  <a role="button" href="blog">Blog</a>
-  <a role="button" href="projects">Projects</a>
+  <p><i>Tell me I'm wrong</i> &#x2192;<a class="contrast" href="#contact">Contact</p>
+  
+  <div class="container">
+    <a role="button" href="blog">Blog</a>
+    <a role="button" href="projects">Projects</a>
+  </div>
 </div>
+
+<br/>
 
 <div class="container" data-theme="light">
   <h1>About Me!</h1>
