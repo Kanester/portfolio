f70451bfee16cb15e1fcbfb9c518128cd5fb818f
KanesterP <pampagkieven091@gmail.com>
2025-05-13 17:26:08 +0800

    --no-message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,12 +19,12 @@
 
 <footer class="container-fluid" data-theme="dark">
   <p>&copy; 2025 Made by Kiö. All rights reserved.</p>
-  <nav>
-    <a href="/portfolio">Home</a>
-    <a href="blog">Blogs</a>
-    <a href="projects">Projects</a>
-    <a href="#contact">Contact</a>
-  </nav>
+  <aside>
+    <a href="/portfolio" class="contrast">Home</a>
+    <a href="blog" class="contrast">Blogs</a>
+    <a href="projects" class="contrast">Projects</a>
+    <a href="#contact" class="contrast">Contact</a>
+  </aside>
   <p>Built with svelte + termux</p>
-  <p>No bugs were harmed during development—only me :)</p>
+  <p>No bugs were harmed during development—only me &#x25C9;&#x2060;&#x203F;&#x2060;&#x25C9;</p>
 </footer>
