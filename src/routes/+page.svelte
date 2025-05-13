f89011e7a08e718a3f1acd47ca7d2b4b30d46711
KanesterP <pampagkieven091@gmail.com>
2025-05-13 17:09:55 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
   </div>
 </section>
 
-<section class="contact" data-theme="light">
+<section class="contact" data-theme="light" id="contact">
   <div class="container">
     <div class="container-fluid">
       <h1>Contact Me</h1>
