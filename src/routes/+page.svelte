4ed0c7292bb2c5c23949e748bb5d36c4a7778c72
KanesterP <pampagkieven091@gmail.com>
2025-05-14 11:51:02 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <section class="container-fluid hero" data-theme="dark">
   <div class="container">
     <hgroup>
-      <h1>Hi y'all! I'm Kiö.</h1>
+      <h1 style="font-family: 'Modak', system-ui">Hi y'all! I'm Kiö.</h1>
       <p>A builder of lots of weird stuff on the web, sometimes on CLI; yet they work&#x2014;somehow.. (&#x2060;&#x665;&#x2060;&#x21BC;&#x2060;_&#x2060;&#x21BC;&#x2060;)</p>
       <p>Programmer, storyteller, and CSS is a nightmare.</p>
       <p>This is my corner of the web where <strong><i>I</i></strong> pretend to know what in <strong>hell</strong> I'm doing...</p>
