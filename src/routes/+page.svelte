c548346c1b62ba02cf8b1916adf4f43baae70db3
KanesterP <pampagkieven091@gmail.com>
2025-05-15 10:29:58 +0800

    --no-message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,36 +23,103 @@
   </div>
 </section>
 
+<section class="now container-fluid" id="now" data-theme="dark">
+  
+</section>
+
 <!-- About -->
 <section class="about container-fluid" id="about" data-theme="dark">
   <h1>About Me!</h1>
   <p>
-    I'm Kiö — a self-taught programmer who wandered into the web and still can’t
-    get CSS to behave.
+    Again I'm Kiö — a self-taught programmer who wandered into the chaotic world of the web; yet still — <abbr title="Chaotic Supplier of Suffering">CSS</abbr> is throwing a lot of tantrum...
   </p>
   <p>
     I build stuff that’s half serious, half chaotic. Everything’s an experiment
-    — from web projects to CLI apps. This site? It’s my playground, my sandbox,
-    my digital mess.
+    — from web projects to CLI apps.
   </p>
   <p>
-    When I’m not lost in code, I’m diving into manhwa, vibing to J-pop, or writing
-    overly complicated stories about broken characters (don’t judge).
+    This site? Just another mess — my sandbox and my playground. Everything here is half experimental yet it's official.
+  </p>
+  <p>
+    When I’m not lost in code, I’m diving into manhwa, vibing to J-pop, or writing overly complicated stories about broken characters (don’t judge).
   </p>
   <p>
     If you’ve read this far, you’re either brave, foolish, or just vibing. Either way,
-    <a href="#contact">reach out</a> and tell me what I’m doing wrong.
+    <a href="#contact">reach out</a> and tell me what I’m doing wrong. I'm open for collaboration and tutoring.
   </p>
+</section>
+
+<section class="skillsetcontainer-fluid" id="skillset" data-theme="light">
+  <h1>My Skillset</h1>
+  <p>Not a master of anything, but here's what I'm jibering around.</p>
+  <ul>
+    <li>
+      <strong>HTML/CSS</strong>:
+      <progress />
+      <p>Intermidiate-ish — CSS still hunts me.</p>
+    </li>
+    <li>
+      <strong>Javascript/Typescript</strong>:
+      <progress value="60"/>
+      <p>Pretty much okay..</p>
+    </li>
+    <li>
+      <strong>Python</strong>:
+      <progress value="40"/>
+      <p>It's good, just doesn't like multiple modules.</p>
+    </li>
+    <li>
+      <strong>CLI</strong>
+      <progress value="50" />
+      <p>That's a strong 50:50</p>
+    </li>
+    <li>
+      <strong>Git/Version Control</strong>
+      <p>Well.. I could <code>git add</code>, <code>git commit</code>, and <code>git push</code> — does that count?</p>
+    </li>
+  </ul>
+</section>
+
+<section class="toolbox container-fluid" id="toolbox" data-theme="light">
+  <h1>My Chaotic Toolset</h1>
+  <p>I'm sure all dev will cry after reading this...</p>
+  <ul>
+    <li>
+      <strong>Termux</strong>
+      <p>My saviour for all the mess i made...</p>
+    </li>
+    <li>
+      <strong>Phone (with android 14)</strong>
+      <p>It clashes with my termux...</p>
+    </li>
+    <li>
+      <strong>Acode (android)</strong>
+      <p>Nice companion — it has cool plugins!</p>
+    </li>
+  </ul>
+  
+  <p>Yea, I'm broke *insert crying emoji because i can't access my html entity generator for now*</p>
+</section>
+
+<section class="why container-fluid" id="why" data-theme="dark">
+  
+</section>
+
+<section class="fun container-fluid" id="fun" data-theme="dark">
+  
 </section>
 
 <!-- Contact -->
 <section class="contact container-fluid" id="contact" data-theme="light">
-  <h1>Contact Me</h1>
+  <h1>Contact Me Using These</h1>
   <ul>
     <li>
       <i>Github</i>:
       <a href="https://github.com/Kanester">Kanester</a>
     </li>
+    <li>
+      <i>Email</i>:
+      <a href="mailto:kiovmryphll@gmail.com">kiovmryphll@gmail.com</a>
+    </li>
   </ul>
-  <p>Still under contstruction</p>
 </section>
