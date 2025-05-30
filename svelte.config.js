8454477f55562b070eee8226260a5941f397c993
KanesterP <pampagkieven091@gmail.com>
2025-05-30 17:23:44 +0800

    --no-message

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,16 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import purgecss from 'vite-plugin-purgecss';
 
 const basePath = process.env.BASE_PATH || '/portfolio';
 
-/** @type {import('sveltejs/kit').Config} **/
 const config = {
-	preprocess: vitePreprocess({
-		scss: {
-			includePaths: ['node_modules']
-		}
-	}),
-
+	preprocess: [vitePreprocess({ scss: { includePaths: ['node_modules'] } }), mdsvex()],
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		paths: {
-			base: basePath
-		}
+		adapter: adapter({ fallback: 'index.html' }),
+		paths: { base: basePath }
 	},
-
 	vite: {
 		plugins: [
 			purgecss({
@@ -29,7 +19,8 @@ const config = {
 				defaultExtractor: (code) => code.match(/[\w-/:]+(?<!:)/g) || []
 			})
 		]
-	}
+	},
+	extensions: ['.svelte', '.svx']
 };
 
 export default config;
